d0d3b2a037a0e10551b3c12f849c2049c45787c1
joo01029 <62934181+joo01029@users.noreply.github.com>
2022-04-01 18:12:25 +0900

    [add] auth register service

---
+++ src/main/java/com/out/android/domain/repo/UserRepo.java
@@ -0,0 +1,10 @@
+package com.out.android.domain.repo;
+
+import com.out.android.domain.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepo extends JpaRepository<User, Long> {
+	Optional<User> findById(String id);
+}
---
+++ src/main/java/com/out/android/domain/request/auth/RegisterDto.java
@@ -0,0 +1,21 @@
+package com.out.android.domain.request.auth;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.NotBlank;
+
+@Getter
+@Setter
+public class RegisterDto {
+	@Length(min = 4, max = 20, message = "아이디의 범위는 4~20글자 사이입니다.")
+	@NotBlank(message = "아이디는 공백이 불가능합니다.")
+	private String id;
+	@Length(min = 8, max = 40, message = "비밀번호의 범위는 8~40글자 사이입니다.")
+	@NotBlank(message = "비밀번호는 공백이 불가능합니다.")
+	private String password;
+	@Length(min = 4, max = 20, message = "이름의 범위는 4~20글자 사이입니다.")
+	@NotBlank(message = "이름은 공백이 불가능합니다.")
+	private String name;
+}
