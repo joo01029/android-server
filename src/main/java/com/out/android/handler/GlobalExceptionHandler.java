90927ea68dfe96dcefd20cfc4e52ddf7695598d5
joo01029 <62934181+joo01029@users.noreply.github.com>
2022-04-01 17:21:48 +0900

    [add] exception handler

---
+++ src/main/java/com/out/android/handler/GlobalExceptionHandler.java
@@ -0,0 +1,26 @@
+package com.out.android.handler;
+
+import com.out.android.domain.response.Response;
+import com.out.android.exception.CustomException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+	@ExceptionHandler(CustomException.class)
+	public ResponseEntity<Response> handleCustomException(CustomException e){
+		Response response = new Response(e.getStatus().value(), e.getMessage());
+
+		return new ResponseEntity<>(response, e.getStatus());
+	}
+
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<Response> handleValidException(MethodArgumentNotValidException e) {
+		Response response = new Response(HttpStatus.BAD_REQUEST.value(), e.getBindingResult().getAllErrors().get(0).getDefaultMessage());
+
+		return new ResponseEntity<Response>(response, HttpStatus.BAD_REQUEST);
+	}
+}
