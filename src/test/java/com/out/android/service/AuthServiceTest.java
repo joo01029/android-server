91eb7d1f5b6f3b6d54b56c4c863eb4c12fc5705c
joo01029 <62934181+joo01029@users.noreply.github.com>
2022-05-09 16:54:55 +0900

    [add] login test

--- src/test/java/com/out/android/service/AuthServiceTest.java
+++ src/test/java/com/out/android/service/AuthServiceTest.java
@@ -2,9 +2,11 @@ package com.out.android.service;
 
 import com.out.android.domain.entity.User;
 import com.out.android.domain.repo.UserRepo;
+import com.out.android.domain.request.auth.LoginDto;
 import com.out.android.domain.request.auth.RegisterDto;
-import com.out.android.service.auth.AuthService;
+import com.out.android.domain.response.auth.LoginResponse;
 import com.out.android.service.auth.AuthServiceImpl;
+import com.out.android.util.JwtProvider;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -14,8 +16,6 @@ import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Optional;
-
-import static org.mockito.BDDMockito.given;
 import static org.mockito.Matchers.any;
 
 @ExtendWith(MockitoExtension.class)
@@ -26,6 +26,10 @@ public class AuthServiceTest {
 	@Mock
 	private UserRepo userRepo;
 
+	@Mock
+	private JwtProvider jwtProvider;
+
+	private String tokenMessage = "token create success";
 	@BeforeEach
 	public void saveEntity(){
 		User user = User.builder()
@@ -34,12 +38,17 @@ public class AuthServiceTest {
 				.name("asdf")
 				.idx(1L)
 				.build();
+		//userRepo
 		Mockito.lenient().when(userRepo.save(any()))
 				.thenReturn(null);
 		Mockito.lenient().when(userRepo.findById("asdf"))
 				.thenReturn(Optional.ofNullable(user));
 		Mockito.lenient().when(userRepo.findById("qwer"))
 				.thenReturn(Optional.empty());
+
+		//jwtProvider
+		Mockito.lenient().when(jwtProvider.encodingToken(1L))
+				.thenReturn(tokenMessage);
 	}
 	@Test
 	public void registerSuccessTest(){
@@ -69,6 +78,32 @@ public class AuthServiceTest {
 			e.printStackTrace();
 			assert (true);
 		}
+	}
 
+	@Test
+	public void loginSuccessTest(){
+		LoginDto loginDto = new LoginDto();
+		loginDto.setId("asdf");
+		loginDto.setPassword("asdf");
+		try{
+			LoginResponse response = authService.login(loginDto);
+			assert(response.getToken().length() > 0);
+		}catch (Exception e){
+			e.printStackTrace();
+			assert(false);
+		}
+	}
+	@Test
+	public void loginFailTest(){
+		LoginDto loginDto = new LoginDto();
+		loginDto.setId("qwer");
+		loginDto.setPassword("asdf");
+		try{
+			LoginResponse response = authService.login(loginDto);
+			assert(false);
+		}catch (Exception e){
+			e.printStackTrace();
+			assert(true);
+		}
 	}
 }
